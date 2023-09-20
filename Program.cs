d40d84dd8fdb4babafa1bffd6d82af5cff80b34e
Vile15 <138338631+Vile15@users.noreply.github.com>
2023-09-19 21:41:18 -0300

    Add files via upload
    Arquivo simples em C#. Digite dois números inteiros e divirta-se com essa coisa aí.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HelloWold;
+
+public static class Program 
+{         
+    public static void Main()
+	
+    {
+		int CRL;
+		int CRL2;
+	    Console.WriteLine("NÚMERO MAIOR,IGUAL OU MENOR");
+		Console.WriteLine("Digite um Número:");
+	    CRL = int.Parse(Console.ReadLine());
+	    Console.WriteLine("Digite outro Número:");
+		CRL2 = int.Parse(Console.ReadLine());
+	
+		if(CRL > CRL2){
+		Console.WriteLine(CRL+" é maior que "+CRL2);
+		}
+		
+		else if (CRL == CRL2){
+		Console.WriteLine(CRL+" é igual que " +CRL2);
+		}
+		
+		else{
+			
+		Console.WriteLine(CRL+ " é menor que " +CRL2);
+		}
+		
+		
+        
+    }
+}
