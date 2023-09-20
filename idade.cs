2ba98e6034e0423b5a14178c6586862950184110
Vile15 <138338631+Vile15@users.noreply.github.com>
2023-09-19 21:46:16 -0300

    Add files via upload
    Pequeno arquivo em C#. Sua função é perguntar a idade de quem digita e dizer se a pessoa é um bebê ,uma criança e por aí vai. Divirta-se com isso.

---
+++ idade.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace jebson;
+
+public class idade 
+{         
+        public static void Main(){
+			
+			
+			
+		Console.WriteLine("Me diga a sua idade:");
+		
+		int idade = int.Parse(Console.ReadLine());
+        
+		if(idade >= 0 && idade <= 4){
+		Console.WriteLine("Você é um bebê.");
+	}
+	
+	    else if(idade >= 5 && idade <= 11){
+		
+		Console.WriteLine("Você é uma criança.");
+	
+		}
+		else if(idade == 12 || idade == 13){
+		
+		Console.WriteLine("Você é Pré-Adolescente.");
+		}
+		
+		else if (idade >= 14 && idade <= 17){
+		
+		Console.WriteLine("Você é um adolescente");
+		}
+		
+		else if (idade >= 18 && idade <= 59){
+		
+		Console.WriteLine("Você é um adulto");
+		}
+		
+		else if (idade >= 60){
+		Console.WriteLine("Você já é um idoso(a)");
+		}
+		
+		else{
+		Console.WriteLine("Apenas númeŕos inteiros são permitidos.");
+		}
+		
+		}
+		}
+		
